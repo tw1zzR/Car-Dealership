f6071b2aa381de441f805d7f82faa6989f51fd7d
tw1zzR <regenuss@gmail.com>
2025-01-30 01:46:03 +0200

    Initialize autos in class dealership instead of another file.

--- Dealership.py
+++ Dealership.py
@@ -1,5 +1,21 @@
 from create_autos import *
+from Cars import *
 
 class Dealership:
+    # BMW
+    bmw_X5 = Crossover("BMW", "X5", 2019, 83, 56300, True, False)
+    bmw_M4 = Sportcar("BMW", "M4", 2018, 59, 55000, False, True)
+    bmw_X7_pickup = Pickup("BMW", "X7 Pick-up", 2021, 88, 45750, True, True)
+
+    # AUDI
+    audi_Q7 = Crossover("Audi", "Q7", 2020, 85, 60000, True, False)
+    audi_R8 = Sportcar("Audi", "R8", 2023, 73, 72500, True, True)
+    audi_activesphere = Pickup("Audi", "Activesphere", 2025, 0, 43200, False, True)
+
+    # HONDA
+    honda_CRV = Crossover("Honda", "CR-V", 2022, 57, 51050, True, True)
+    honda_NSX = Sportcar("Honda", "NSX", 2015, 59, 34600, False, True)
+    honda_Ridgeline = Pickup("Honda", "Ridgeline", 2024, 73, 31900, True, False)
+
     def __init__(self):
         print("Initializing Dealership")
